f127882ee810154b527e31bcda5c2aa49b03c619
Luis Mendes <cvlopes88@gmail.com>
2019-11-15 14:30:19 -0500

    most

--- api/server.js
+++ api/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require("express-session"); // 1: npm i express-session
+const KnexSessionStorage = require("connect-session-knex")(session); // <<<<<< for storing sessions in db
+
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const knexConnection = require('../database/dbConfig');
+const userRouter = require('../users/usersRouter');
 
 const server = express();
 
+server.use('/api/users', userRouter);
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+
+server.get("/", (req, res) => {
+    res.json({ api: "up"});
+  });
 
 module.exports = server;
